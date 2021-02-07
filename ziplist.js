5acf46251a9c9036ab8c18f0b10eeea713d4f870
Cris Javier <crisj@hawaii.edu>
2021-02-06 22:01:15 -1000

    Finished WOD

---
+++ ziplist.js
@@ -0,0 +1,16 @@
+function zipList(arr1, arr2) {
+  const list = [];
+  for (let i = 0; i < arr1.length && i < arr2.length; i++) {
+    list.push(arr1[i]);
+    list.push(arr2[i]);
+  }
+  return list;
+}
+
+function zipListTheSimpleWay(arr1, arr2) {
+  const list = _.zip(arr1, arr2);
+  return _.flatten(list);
+}
+
+console.log(zipList(['a', 'b', 'c'], [1, 2, 3]));
+console.log(zipListTheSimpleWay(['a', 'b', 'c'], [1, 2, 3]));
